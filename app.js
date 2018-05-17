6950c1993550e4aa1c81353b5c6e0c889af87ee7
Garnett Strack <garnett.strack@gmail.com>
2018-05-16 23:50:22 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+app.config = require("./config.json");
+
+var tmi = require('tmi.js');
+
+var options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        cluster: "aws",
+        reconnect: true
+    },
+    identity: {
+        username: "ZapteccBot",
+        password: app.config.password
+    },
+    channels: ["Zaptecc"]
+};
+
+var client = new tmi.client(options);
+client.connect();
+
+client.on('connected', function(address, port) {
+    console.log("Address: " + address + ", Port: " + port);
+    client.action("Zaptecc", "Connected to chat!")
+});
+
+client.on('chat', function(channel, user, message, self) {
+    client.action("Zaptecc", user['display-name'] + " you rang?")
+})
